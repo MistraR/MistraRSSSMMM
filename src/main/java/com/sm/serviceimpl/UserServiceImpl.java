8fccc7d4e88f52358f28ed7fc8f9a129eda4def3
MistraR <842404548@qq.com>
2017-11-13 23:03:11 +0800

    serviceimpl

--- src/main/java/com/sm/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/sm/serviceimpl/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.sm.dao.UserMapper;
 import com.sm.po.User;
 import com.sm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 /**
  * Created by Mistra.WR on 2017/11/10/010.
  */
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
